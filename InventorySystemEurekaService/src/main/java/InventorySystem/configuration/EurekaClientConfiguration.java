4858aa731cf9b3eee7def343f24f26ea2658a0c5
Sasu <sasuolander@yahoo.com>
2019-04-15 19:27:11 +0300

    adding swagger documentation

---
+++ InventorySystemEurekaService/src/main/java/InventorySystem/configuration/EurekaClientConfiguration.java
@@ -0,0 +1,11 @@
+package inventorysystem.configuration;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!development")
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfiguration {
+}
