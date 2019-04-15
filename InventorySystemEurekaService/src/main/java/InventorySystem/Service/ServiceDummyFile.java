6032d500957d12701ee958eab11f94a605352ad0
Sasu <sasuolander@yahoo.com>
2019-04-15 12:06:34 +0300

    redesigning endpoint to start with /api

--- InventorySystemEurekaService/src/main/java/InventorySystem/Service/ServiceDummyFile.java
+++ InventorySystemEurekaService/src/main/java/InventorySystem/Service/ServiceDummyFile.java
@@ -1,7 +1,9 @@
-package inventorysystem.Service;
+package inventorysystem.service;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceDummyFile {
 }
+
+//https://docs.spring.io/spring-data/rest/docs/current/reference/html/#customizing-sdr.overriding-sdr-response-handlers
