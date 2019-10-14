dc4fad57a692e30c82f2d58ea086d6451781897e
Sasu <sasuolander@yahoo.com>
2019-10-14 15:36:32 +0300

    updating zuul service

--- UserLoginEurekaService/src/main/java/main/security/SecurityConstant.java
+++ UserLoginEurekaService/src/main/java/main/security/SecurityConstant.java
@@ -7,7 +7,7 @@ public final class SecurityConstant {
     public static final long EXPIRATION_TIME=864000000;
     // Signing key for HS512 algorithm
     // You can use the page http://www.allkeysgenerator.com/ to generate all kinds of keys
-    public static final String JWT_SECRET = "n2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";
+    public static final String JWT_SECRET = "n2u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";
 
     // JWT token defaults
     public static final String TOKEN_HEADER = "Authorization";
